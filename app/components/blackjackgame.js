31d8747093e78bc766c871a2cfa54591bc9f8b2e
Alex Taylor <alextaylor@M3777WLXDH.lan>
2023-01-18 15:16:16 +0000

    Add player hit and stand functionality

--- app/components/blackjackgame.js
+++ app/components/blackjackgame.js
@@ -4,29 +4,36 @@ import { action } from '@ember/object';
 
 export default class BlackjackgameComponent extends Component {
   suits = ['club', 'heart', 'spade', 'diamond'];
-  dealersTurn = false;
 
   @tracked dealerCards = [];
   @tracked playerCards = [];
+  @tracked dealersTurn = false;
+  @tracked playersTurn = false;
+  @tracked gameOver = false;
+  @tracked gameResultMessage = '';
 
   @action
   dealCard() {
     const cardClub = this.suits[Math.floor(Math.random() * this.suits.length)];
-    const cardValue = Math.floor(Math.random() * 13) + 1;
-    const imgUri = `assets/images/cards/${cardClub}${cardValue}.svg`;
+    const cardNumber = Math.floor(Math.random() * 13) + 1;
+    const imgUri = `assets/images/cards/${cardClub}${cardNumber}.svg`;
+    const cardValue = cardNumber > 10 ? 10 : cardNumber;
 
     return {
       club: cardClub,
-      value: cardValue,
+      number: cardNumber,
       img_uri: imgUri,
+      value: cardValue,
     };
   }
 
   @action
   resetGame() {
+    this.playersTurn = true;
     this.dealersTurn = false;
     this.dealerCards = [];
     this.playerCards = [];
+    this.gameOver = false;
   }
 
   @action
@@ -39,6 +46,45 @@ export default class BlackjackgameComponent extends Component {
 
     for (let i = 0; i <= 1; i++) {
       this.dealerCards.push(this.dealCard());
+
+      if (this.calculateTotalHandValue(this.playerCards) === 21) {
+        this.gameOver = true;
+        this.gameResultMessage = 'Black jack!, Player Wins!';
+      }
     }
+  }
+
+  @action
+  calculateTotalHandValue(cards) {
+    let total = 0;
+
+    for (let i = 0; i < cards.length; i++) {
+      total += cards[i].value;
+    }
+    return total;
+  }
+
+  @action
+  endGame() {
+    this.gameOver = true;
+    this.playersTurn = false;
+    this.dealersTurn = false;
+  }
+
+  @action
+  drawPlayerCard() {
+    this.playerCards = [...this.playerCards, this.dealCard()];
+    const playersHandTotal = this.calculateTotalHandValue(this.playerCards);
+
+    if (playersHandTotal > 21) {
+      this.endGame();
+      this.gameResultMessage = 'Player Bust, Dealer Wins';
+    }
+  }
+
+  @action
+  endPlayersTurn() {
+    this.playersTurn = false;
+    this.dealersTurn = true;
   }
 }
